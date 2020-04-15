f0f71efe582e38ba42a14241d382348e0f02f9f6
Johanna Farace <Johanna.farace@gmail.com>
2020-04-15 10:47:26 +0200

    User registration

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const { check, validationResult } = require('express-validator/check');
+const gravatar = require('gravatar');
+const bcrypt = require('bcryptjs');
+const { check, validationResult } = require('express-validator'); // /check
+
+const User = require('../../models/User');
 
 // @route   Post api/UserSchema
 // @desc    Register user
@@ -16,12 +20,50 @@ router.post('/',
     'password',
     'Please enter a password with 6 or more characters')
     .isLength({min: 6})
-], (req, res) => {
+],
+async (req, res) => {
   const errors = validationResult(req);
   if(!errors.isEmpty()) {
     return res.status(400).json({ errors: errors.array()});
   }
-  res.send('User route')
+  const { name, email, password } = req.body;
+
+  try {
+    // since we destructured email from req.body we don't have to do email: email (same name just use once)
+    let user = await User.findOne({ email });
+
+    if(user) {
+      // since for the other err msg we have an array of objs in check we add this one in this "funny" way
+      return res
+      .status(400)
+      .json({ errors: [{ msg: 'User already exists' }] });
+    }
+
+    const avatar = gravatar.url(email, {
+      s: '200',
+      r: 'pg',
+      d: 'mm'
+    });
+
+    user = new User({
+      name,
+      email,
+      avatar,
+      password
+    });
+
+    const salt = await bcrypt.genSalt(10);
+
+    user.password = await bcrypt.hash(password, salt);
+
+    await user.save();
+
+    res.send('User registered');
+  } catch(err){
+    console.error(err.message);
+    res.status(500).send('Server error');
+  }
+
 });
 
 module.exports = router;

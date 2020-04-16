a9b3537945597a2b3d45db3597339739a086a6fc
Johanna Farace <Johanna.farace@gmail.com>
2020-04-16 19:44:59 +0200

    Create and update profile routes

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const auth = require('../../middleware/auth');
+const { check, validationResult } = require('express-validator'); // /check
 
 const Profile = require('../../models/Profile');
 const User = require('../../models/User');
@@ -22,6 +23,87 @@ router.get('/me', auth, async (req, res) => {
   } catch(err){
     console.error(err.message);
     res.status(500).send('Server error');
+  }
+});
+
+// @route   POST api/profile
+// @desc    Create or update user profile
+// @access  Private
+// auth and check are the middleware in this case
+router.post('/', [ auth, [
+  check('status', 'Status is required.')
+    .not()
+    .isEmpty(),
+  check('skills', 'Skills is required')
+  .not()
+  .isEmpty()
+  ]
+],
+async (req, res) => {
+  const errors = validationResult(req);
+  if(!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array()});
+  }
+  // destructuring req.body
+  const {
+    company,
+    website,
+    location,
+    bio,
+    status,
+    githubusername,
+    skills,
+    youtube,
+    twitter,
+    facebook,
+    linkedin,
+    instagram
+  } = req.body;
+
+  // Build profile object
+  const profileFields = {};
+  // We have a reference to the user req.user.id linked in the Profile model
+  profileFields.user = req.user.id;
+  if(company) profileFields.company = company;
+  if(website) profileFields.website = website;
+  if(bio) profileFields.bio = bio;
+  if(status) profileFields.status = status;
+  if(githubusername) profileFields.githubusername = githubusername;
+  if(skills) {
+    // we have to turn a sting into an array and trim it in case of spaces after the comma
+    profileFields.skills = skills.split(',').map(skill => skill.trim());
+  }
+  // Build social object
+  profileFields.social = {};
+  // if we don't add that social is an obj it will return an error saying cannot find youtube of undefined
+  if(youtube) profileFields.social.youtube = youtube;
+  if(twitter) profileFields.social.twitter = twitter;
+  if(facebook) profileFields.social.facebook = facebook;
+  if(linkedin) profileFields.social.company = linkedin;
+  if(instagram) profileFields.social.instagram = instagram;
+
+  try {
+    let profile = await Profile.findOne({ user: req.user.id });
+
+    if(profile) {
+      // Update
+      profile = await Profile.findOneAndUpdate(
+        { user: req.user.id },
+        { $set: profileFields},
+        { new: true }
+      );
+
+      return res.json(profile);
+    }
+
+    // Create
+    profile = new Profile(profileFields);
+
+    await profile.save();
+    res.json(profile);
+  } catch(err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
   }
 });
 

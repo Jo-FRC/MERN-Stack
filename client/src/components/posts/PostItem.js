92a4ea1ff679633650c5288226ca66007b616ec5
Johanna Farace <Johanna.farace@gmail.com>
2020-05-01 18:28:00 +0200

    Adding posts

--- client/src/components/posts/PostItem.js
+++ client/src/components/posts/PostItem.js
@@ -22,45 +22,45 @@ const PostItem = ({
   }
 }) => {
   return (
-    <div class="post bg-white p-1 my-1">
+    <div className="post bg-white p-1 my-1">
       <div>
-        <a href="profile.html">
+        <Link to={`/profile/${user}`}>
           <img
-            class="round-img"
+            className="round-img"
             src={avatar}
             alt=""
           />
           <h4>{name}</h4>
-        </a>
+        </Link>
       </div>
       <div>
-        <p class="my-1">
+        <p className="my-1">
           {text}
         </p>
-         <p class="post-date">
+         <p className="post-date">
             Posted on <Moment format="DD/MM/YYYY">{date}</Moment>
         </p>
-        <button onClick={() => addLike(_id)} type="button" class="btn btn-light">
-          <i class="fas fa-thumbs-up"></i>{' '}
+        <button onClick={() => addLike(_id)} type="button" className="btn btn-light">
+          <i className="fas fa-thumbs-up"></i>{' '}
           {likes.length > 0 && (
             <span>{likes.length}</span>
           )}
         </button>
-        <button  onClick={() => removeLike(_id)} type="button" class="btn btn-light">
-          <i class="fas fa-thumbs-down"></i>
+        <button  onClick={() => removeLike(_id)} type="button" className="btn btn-light">
+          <i className="fas fa-thumbs-down"></i>
         </button>
-        <Link to={`/posts/${_id}`} class="btn btn-primary">
+        <Link to={`/posts/${_id}`} className="btn btn-primary">
           Discussion {' '}
           {comments.length > 0 && (
-            <span class='comment-count'>{comments.length}</span>
+            <span className='comment-count'>{comments.length}</span>
           )}
         </Link>
         {!auth.loading && user === auth.user._id && (
           <button
           onClick={() => deletePost(_id)}
           type="button"
-          class="btn btn-danger">
-          <i class="fas fa-times"></i>
+          className="btn btn-danger">
+          <i className="fas fa-times"></i>
         </button>
         )}
       </div>
